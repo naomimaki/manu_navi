6e48bcb9ec249b097018d640df7319f4f0769b85
naomimaki <adoresuhakore11@gmail.com>
2020-09-29 13:21:14 +0900

    topページを表示する

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  root to: 'welcome#index'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: [:new, :index]
+  resources :manuals, only: [:index, :new, :show]
+  resources :contacts, only: :index
+  resources :welcome, only: :index
 end
